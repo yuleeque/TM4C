44c3814615b886b0117b88c40b2e613d5a456a03
Yulien Khlevnyi <yulian.khlevnoy@gmail.com>
2023-05-21 14:59:12 +0200

    Add sysctl.h

--- lcd1602/main.c
+++ lcd1602/main.c
@@ -1,9 +1,11 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/sysctl.h"  // two prev define "uint32_t"s and "bool"s occurred in sysctl.h
 
 
-/**
- * main.c
- */
+
+
 int main(void)
 {
-	return 0;
+    return 0;
 }
